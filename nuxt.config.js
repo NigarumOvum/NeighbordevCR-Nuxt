e4fcfd9efc2dc40c1adb478137299f181264868c
nigarumovum <bfpr131095@gmail.com>
2022-03-12 15:13:34 -0600

    server port fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,60 +11,129 @@ module.exports = {
   */
   head: {
     htmlAttrs: {
-      dir: 'ltr'
+      dir: "ltr",
     },
     title: brand.starter.name,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'description', content: brand.starter.desc },
-      { name: 'msapplication-TileColor', content: '#FFFFFF' },
-      { name: 'msapplication-TileImage', content: '/favicons/ms-icon-144x144.png' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "description", content: brand.starter.desc },
+      { name: "msapplication-TileColor", content: "#FFFFFF" },
+      {
+        name: "msapplication-TileImage",
+        content: "/favicons/ms-icon-144x144.png",
+      },
       // PWA primary color
-      { name: 'theme-color', content: theme.primary },
+      { name: "theme-color", content: theme.primary },
       // Facebook
-      { property: 'author', content: 'neighbordev' },
-      { property: 'og:site_name', content: 'neighbordevcr.com' },
-      { property: 'og:locale', content: 'en_US' },
-      { property: 'og:type', content: 'website' },
+      { property: "author", content: "neighbordev" },
+      { property: "og:site_name", content: "neighbordevcr.com" },
+      { property: "og:locale", content: "en_US" },
+      { property: "og:type", content: "website" },
       // Twitter
-      { property: 'twitter:site', content: 'neighbordevcr' },
-      { property: 'twitter:domain', content: 'neighbordevcr' },
-      { property: 'twitter:creator', content: 'neighbordev' },
-      { property: 'twitter:card', content: 'summary' },
-      { property: 'twitter:image:src', content: '/images/logo.png' },
-      { property: 'og:url', content: brand.starter.url },
-      { property: 'og:title', content: brand.starter.projectName },
-      { property: 'og:description', content: brand.starter.desc },
-      { name: 'twitter:site', content: brand.starter.url },
-      { name: 'twitter:card', content: 'summary_large_image' },
-      { name: 'twitter:image', content: brand.starter.img },
-      { property: 'og:image', content: brand.starter.img },
-      { property: 'og:image:width', content: '1200' },
-      { property: 'og:image:height', content: '630' }
+      { property: "twitter:site", content: "neighbordevcr" },
+      { property: "twitter:domain", content: "neighbordevcr" },
+      { property: "twitter:creator", content: "neighbordev" },
+      { property: "twitter:card", content: "summary" },
+      { property: "twitter:image:src", content: "/images/logo.png" },
+      { property: "og:url", content: brand.starter.url },
+      { property: "og:title", content: brand.starter.projectName },
+      { property: "og:description", content: brand.starter.desc },
+      { name: "twitter:site", content: brand.starter.url },
+      { name: "twitter:card", content: "summary_large_image" },
+      { name: "twitter:image", content: brand.starter.img },
+      { property: "og:image", content: brand.starter.img },
+      { property: "og:image:width", content: "1200" },
+      { property: "og:image:height", content: "630" },
     ],
     link: [
       // Favicon
-      { rel: 'shortcut icon', href: '/favicons/favicon.ico' },
-      { rel: 'apple-touch-icon', sizes: '57x57', href: '/favicons/apple-icon-57x57.png' },
-      { rel: 'apple-touch-icon', sizes: '60x60', href: '/favicons/apple-icon-60x60.png' },
-      { rel: 'apple-touch-icon', sizes: '72x72', href: '/favicons/apple-icon-72x72.png' },
-      { rel: 'apple-touch-icon', sizes: '76x76', href: '/favicons/apple-icon-76x76.png' },
-      { rel: 'apple-touch-icon', sizes: '114x114', href: '/favicons/apple-icon-114x114.png' },
-      { rel: 'apple-touch-icon', sizes: '120x120', href: '/favicons/apple-icon-120x120.png' },
-      { rel: 'apple-touch-icon', sizes: '144x144', href: '/favicons/apple-icon-144x144.png' },
-      { rel: 'apple-touch-icon', sizes: '152x152', href: '/favicons/apple-icon-152x152.png' },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-icon-180x180.png' },
-      { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/favicons/android-icon-192x192.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicons/favicon-96x96.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' },
-      { rel: 'manifest', href: '/favicons/manifest.json' },
+      { rel: "shortcut icon", href: "/favicons/favicon.ico" },
+      {
+        rel: "apple-touch-icon",
+        sizes: "57x57",
+        href: "/favicons/apple-icon-57x57.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "60x60",
+        href: "/favicons/apple-icon-60x60.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "72x72",
+        href: "/favicons/apple-icon-72x72.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "76x76",
+        href: "/favicons/apple-icon-76x76.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "114x114",
+        href: "/favicons/apple-icon-114x114.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "120x120",
+        href: "/favicons/apple-icon-120x120.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "144x144",
+        href: "/favicons/apple-icon-144x144.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "152x152",
+        href: "/favicons/apple-icon-152x152.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/favicons/apple-icon-180x180.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "192x192",
+        href: "/favicons/android-icon-192x192.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicons/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "96x96",
+        href: "/favicons/favicon-96x96.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicons/favicon-16x16.png",
+      },
+      { rel: "manifest", href: "/favicons/manifest.json" },
       // Fonts and Icons
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat:400,500,600&display=swap' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' },
-      { rel: 'stylesheet', href: 'https://unpkg.com/ionicons@3.0.0/dist/css/ionicons.min.css' }
-    ]
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Montserrat:400,500,600&display=swap",
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+      },
+      {
+        rel: "stylesheet",
+        href: "https://unpkg.com/ionicons@3.0.0/dist/css/ionicons.min.css",
+      },
+    ],
   },
   /*
   ** Customize the progress-bar color
@@ -74,41 +143,41 @@ module.exports = {
   ** Global CSS
   */
   css: [
-    '~/assets/transition.scss',
-    '~/assets/vuetify-overide.scss',
-    '~/assets/vendors/animate.css',
-    '~/assets/vendors/animate-extends.css',
-    '~/assets/vendors/hamburger-menu.css',
-    '~/assets/vendors/slick-carousel/slick.css',
-    '~/assets/vendors/slick-carousel/slick-theme.css'
+    "~/assets/transition.scss",
+    "~/assets/vuetify-overide.scss",
+    "~/assets/vendors/animate.css",
+    "~/assets/vendors/animate-extends.css",
+    "~/assets/vendors/hamburger-menu.css",
+    "~/assets/vendors/slick-carousel/slick.css",
+    "~/assets/vendors/slick-carousel/slick-theme.css",
   ],
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '~/plugins/vue-fragment-config',
-    '~/plugins/vue-wow-config',
-    { src: '~plugins/i18n-config.js' },
-    { src: '~/plugins/vue-youtube-config', ssr: false },
-    { src: '~/plugins/vue-lightbox-config', ssr: false },
-    { src: '~/plugins/caroussel-config', ssr: false },
-    { src: '~/plugins/countup-config', ssr: false },
-    { src: '~/plugins/vue-scroll-nav', ssr: false }
+    "~/plugins/vue-fragment-config",
+    "~/plugins/vue-wow-config",
+    { src: "~plugins/i18n-config.js" },
+    { src: "~/plugins/vue-youtube-config", ssr: false },
+    { src: "~/plugins/vue-lightbox-config", ssr: false },
+    { src: "~/plugins/caroussel-config", ssr: false },
+    { src: "~/plugins/countup-config", ssr: false },
+    { src: "~/plugins/vue-scroll-nav", ssr: false },
   ],
   /*
   ** Nuxt.js dev-modules
   */
-  buildModules: ['@nuxtjs/vuetify'],
+  buildModules: ["@nuxtjs/vuetify"],
   /*
   ** Nuxt.js modules
   */
   modules: [
-    ['@nuxtjs/html-minifier', { log: 'once', logHtml: true }],
+    ["@nuxtjs/html-minifier", { log: "once", logHtml: true }],
     [
-      'nuxt-mq',
+      "nuxt-mq",
       {
         // Default breakpoint for SSR
-        defaultBreakpoint: 'default',
+        defaultBreakpoint: "default",
         breakpoints: {
           xsDown: 599,
           xsUp: 600,
@@ -118,34 +187,34 @@ module.exports = {
           mdUp: 1280,
           lgDown: 1919,
           lgUp: 1920,
-          xl: Infinity
-        }
-      }
+          xl: Infinity,
+        },
+      },
     ],
-    ['nuxt-gmaps', { key: '' }],
-    '@nuxtjs/i18n',
+    ["nuxt-gmaps", { key: "" }],
+    "@nuxtjs/i18n",
   ],
   i18n: {
-    lazy:true,
+    lazy: true,
     locales: languages,
-    defaultLocale: 'es',
+    defaultLocale: "es",
     vueI18n: {
-      fallbackLocale: 'es',
+      fallbackLocale: "es",
     },
-    strategy: 'prefix',
+    strategy: "prefix",
     detectBrowserLanguage: {
       useCookie: true,
-      cookieCrossOrigin: true
+      cookieCrossOrigin: true,
     },
-    langDir: 'static/lang/'
+    langDir: "static/lang/",
   },
   /*
   ** vuetify module configuration
   ** https://github.com/nuxt-community/vuetify-module
   */
   vuetify: {
-    customVariables: ['~/assets/styles.scss'],
-    optionsPath: './config/vuetify.options.js'
+    customVariables: ["~/assets/styles.scss"],
+    optionsPath: "./config/vuetify.options.js",
   },
   /*
   ** Render configuration
@@ -154,18 +223,18 @@ module.exports = {
     bundleRenderer: {
       directives: {
         shouldPreload: (file, type) => {
-          return ['script', 'style', 'font'].includes(type)
+          return ["script", "style", "font"].includes(type);
         },
-        scroll: function (el, binding) {
-          let f = function (evt) {
+        scroll: function(el, binding) {
+          let f = function(evt) {
             if (binding.value(evt, el)) {
-              window.removeEventListener('scroll', f)
+              window.removeEventListener("scroll", f);
             }
-          }
-          window.addEventListener('scroll', f)
-        }
-      }
-    }
+          };
+          window.addEventListener("scroll", f);
+        },
+      },
+    },
   },
   /*
   ** Build configuration
@@ -177,12 +246,12 @@ module.exports = {
     // cssSourceMap: false,
     loaders: {
       vus: { cacheBusting: true },
-      scss: { sourceMap: false }
+      scss: { sourceMap: false },
     },
     extend(config, ctx) {
       config.plugins.push(
         new FilterWarningsPlugin({
-          exclude: /Critical dependency: the request of a dependency is an expression/
+          exclude: /Critical dependency: the request of a dependency is an expression/,
         })
       );
       if (ctx.isDev && ctx.isClient) {
@@ -192,29 +261,29 @@ module.exports = {
           loader: "eslint-webpack-plugin",
           exclude: /([node_modules, static])/,
           options: {
-            fix: false
-          }
+            fix: false,
+          },
         });
       }
-    }
+    },
   },
   /*
   ** Page Layout transition
   */
   layoutTransition: {
-    name: 'layout',
-    mode: 'out-in',
+    name: "layout",
+    mode: "out-in",
     beforeEnter(el) {
-      console.log('Before enter...');
+      console.log("Before enter...");
     },
     afterLeave(el) {
-      console.log('afterLeave', el)
-    }
+      console.log("afterLeave", el);
+    },
   },
   /*
   ** Application Port
   */
   server: {
-    port: 8000, // default: 3000
-  }
-}
+    port: process.env.PORT || 8000, // default: 3000
+  },
+};
